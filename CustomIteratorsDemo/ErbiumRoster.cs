bb15dabb61c83803e9c7331d952d935bb712b19e
jjvega86 <jj.vega86@gmail.com>
2020-11-24 18:56:44 -0500

    Created ErbiumRoster class and set member variables/initialized constructor

---
+++ CustomIteratorsDemo/ErbiumRoster.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomIteratorsDemo
+{
+    class ErbiumRoster
+    {
+        private string[] classMembers;
+        private string[] classStatus;
+
+        public ErbiumRoster()
+        {
+            classMembers = new string[] { "Denis", "Greg", "Joe", "Pa", "Zack B", "Zach I" };
+            classStatus = new string[] { "Amazing", "Awesome", "Fantastic", "Rad" };
+        }
+
+
+    }
+}
