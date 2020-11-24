7639a7d1c1da3e8cce91293e01d0d8ea7b420d72
jjvega86 <jj.vega86@gmail.com>
2020-11-24 18:40:20 -0500

    Add project files.

---
+++ CustomIteratorsDemo/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomIteratorsDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
